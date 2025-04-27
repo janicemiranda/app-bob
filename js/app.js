8b39ab04b5c8964f49a8e3c4f75ae61eb460dc50
Janice Miranda <mirandajanice05@gmail.com>
2025-04-26 20:16:12 -0500

    functions for client stats

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+// Dashboard selectors
+const totalClienteStats = document.getElementById('totalClientes');
+const totalClientesHoy = document.getElementById('clientesHoy');
+const totalGarantias = document.getElementById('totalGarantias');
+const totalGarantiasHoy = document.getElementById('garantiasHoy');
+
 // Selectors for the mobile menu toggle
 const menuToggle = document.getElementById('menu-toggle');
 const mobileMenu = document.getElementById('mobile-menu');
@@ -7,6 +13,13 @@ menuToggle.addEventListener('click', () => {
 	mobileMenu.classList.toggle('hidden');
 });
 
+function getYearMonthDay() {
+	const today = new Date();
+	const day = today.getDate();
+	const month = today.getMonth(); // 0-based index, matches your meses array
+	const year = today.getFullYear();
+	return { year, month, day };
+}
 // Minor function to update the current date in the dashboard
 const meses = [
 	'Enero',
@@ -26,13 +39,45 @@ const dias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', '
 
 function setFechaActual() {
 	const hoy = new Date();
-	const diaSemana = dias[hoy.getDay()];
-	const dia = hoy.getDate();
-	const mes = meses[hoy.getMonth()];
-	const año = hoy.getFullYear();
+	const today = getYearMonthDay();
 
-	const fechaFormateada = `${diaSemana} ${dia} de ${mes} ${año}`;
+	const diaSemana = dias[hoy.getDay()];
+	const mes = meses[today.month];
+
+	const fechaFormateada = `${diaSemana} ${today.day} de ${mes} ${today.year}`;
 	document.getElementById('current-date').textContent = fechaFormateada;
 }
 
 setFechaActual();
+
+// Function to update the total number of clients
+document.addEventListener('DOMContentLoaded', () => {
+	// Get the total number of clients
+	const clients = window.getAllClients();
+	console.log(JSON.stringify(clients));
+	const totalClients = clients ? clients.length : 0;
+
+	// Update the DOM element with the total number of clients
+	if (totalClienteStats) {
+		totalClienteStats.textContent = totalClients;
+	}
+	if (totalClientesHoy) {
+		const today = getYearMonthDay();
+		const clientsToday = clients.filter((client) => {
+			const clientDate = new Date(client.createdAt);
+			return (
+				clientDate.getDate() === today.day &&
+				clientDate.getMonth() === today.month &&
+				clientDate.getFullYear() === today.year
+			);
+		});
+		totalClientesHoy.textContent = clientsToday.length;
+	}
+
+	const tranasacciones = window.getClientTransactions();
+	console.log(JSON.stringify(tranasacciones));
+	const totalTranasacciones = clients ? clients.length : 0;
+	if (totalGarantias) {
+		totalGarantias.textContent = totalClients;
+	}
+});
